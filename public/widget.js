31d94b1151ca2069abd1098b67ee1eea5c07b686
proteinaludica <info@proteinaludica.com>
2026-06-21 02:18:25 +0100

    Update fetch body parameters in widget.js

--- public/widget.js
+++ public/widget.js
@@ -209,8 +209,8 @@
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          message: text,
-          history: history.slice(0, -1),
+          userMessage: text,
+          tier: "free",
         }),
       });
 
